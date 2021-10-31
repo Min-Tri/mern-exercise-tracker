1d2d5884e7c33d96170fccd4caeb514b4d3c25b1
Min-Tri <56967379+Min-Tri@users.noreply.github.com>
2021-10-31 21:55:52 +0700

    fontend

---
+++ fontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router,Switch, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.js"
+import ExercisesList from "./components/exercises-list.js";
+import EditExercise from "./components/edit-exercise.js";
+import CreateExercise from "./components/create-exercise.js";
+import CreateUser from "./components/create-user.js";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Switch>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
